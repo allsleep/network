da9d5bb949061a913369679adff9a80c89ebdd3d
linhan <linhan1208@outlook.com>
2021-09-15 17:03:12 +0800

    1

---
+++ p1.cpp
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+int main(int argc, char* argv[]){
+   
+    if (agrc < 3 || argc > 4)
+        DieWithUserMessage("Parameter(s)",
+            "<Server Address> <Echo Word> [<Server Port>]");
+    
+    char *servIP = argv[1];
+    char *echoString = argv[2];
+    
+    // third parm is optional, default parm is 7 that unknow port
+    in_port_t servPort = (argv == 4)? atoi(argv[3]) : 7;
+
+    // create a reliable, stream socket using tcp
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+    if (sock < 0)
+        DieWithSystemMessage("socket() failed");
+    
+    // construct the server address structure
+    struct sockaddr_in servAddr;
+    memset(&servAddr, 0, sizeof(servAddr));
+    servAddr.sin_family = AF_INET;
+
+    // convert address
+    int rtnVal = inet_pton(AF_INET, servIP, &servAddr.sin_addr.s_addr);
+    if (rtnVal == 0)
+        DieWithSystemMessage("inet_pton() failed", "invalid address string");
+    else if (rtnVal < 0)
+        DieWithUserMessage("inget_potn() failed");
+
+    servAddr.sin_port = htons(servPort);
+
+    // establish the connect to the echo server
+    if (connect(sock, (struct sockaddr*) &servAddr, sizeof(servAddr)) < 0)
+        DieWithUserMessage("connect() failed");
+    
+    size_t echoStringLen = strlen(echoString);
+
+    // send the string to the server
+    ssize_t numBytes = send(sock, echoString, echoStringLen, 0);
+    if (numBytes < 0)
+        DieWithSystemMessage("send() failed");
+    else if (numBytes != echoStringLen)
+        DieWithUserMessage("send()", "send unexperted number of bytes");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
