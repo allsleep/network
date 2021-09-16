5edd8822d9c71f84eafc64580afb472ba619f836
linhan <linhan1208@outlook.com>
2021-09-16 09:32:46 +0800

    1

--- p2.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __P2_hpp
-#define __P2_hpp
-
-#define BUFSIZE 64
-
-void DieWithUserMessage(const char *msg, const char *detail);
-void DieWithSystemMessage(const char *msg , const char *detail);
-
-#endif
