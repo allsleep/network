0ecea5979e9e0befefba8d6ac4288f64239bc14b
linhan <linhan1208@outlook.com>
2021-09-30 17:04:49 +0800

    win7

--- address_resolution/getAddrInfo.c
+++ address_resolution/getAddrInfo.c
@@ -3,6 +3,31 @@
 #include <string.h>
 #include <netdb.h>
 
+void PrintSocketAddress(const struct sockaddr *address, FILE *stream){
+    if (address == NULL || stream == NULL)
+        return;
+    
+    void numericAddress; // pointer to binary address
+    // buffer to contain result , IPv6 sufficient to hold IPv4
+    char addrBuffer[INET6_ADDRSTRLEN];
+    in_port_t port; // port to print
+
+    // set pointer to address based on address family
+    switch (address->sa_family) {
+        case AF_INET:
+            numericAddress = &((struct sockaddr_in *) address)->sin_addr;
+            port = ntohs(((struct sockaddr_in *) address)->sin_port);
+            break;
+        case AF_INET6:
+            numericAddress = &((struct sockaddr_in6 *) address)->sin6_addr;
+            port = ntohs(((struct sockaddr_in6 *) address)->sin6_port);
+            break;
+        default:
+            fputs("unknow type", stream);
+        return;
+    }
+}
+
 int main(int argc, char *argv[]){
     if (argc != 3)
         return -1; // DieWithUserMessage("parameter", "<address/name> <port/service>")
