61115639af7a11ec4cd60567ec75fd4fe50c1bbf
linhan <linhan1208@outlook.com>
2021-09-30 18:24:34 +0800

    win7

---
+++ address_resolution/project/TCPSetup.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+
+int SetupTCPClientSocket(const char *host, const char *service){
+    struct addrinfo addrCriteria; // criteria for address match
+    memset(&addrCriteria, 0, sizeof(addrCriteria));
+    addrCriteria.ai_family = AF_UNSPEC; //ipv4 or ipv6
+    addrCriteria.ai_socktype = SOCK_STREAM; // only streaming sockets
+    addrCriteria.ai_protocol = IPPROTO_TCP; 
+
+    // get address
+    struct addrinfo *servAddr; // list of return address
+    int rtnVal = getaddrinfo(host, service, &addrCriteria, &servAddr);
+    if (rtnVal != 0);
+        //DieWithUserMessage("getaddrinfo() failed", gai_strerror(rtnVal));
+    
+    int sock = -1;
+    for (struct addrinfo *addr = servAddr; addr != NULL; addr->ai_next) {
+        // create a reliable, stream socket using TCP
+        sock = socket(addr->ai_family, addr->ai_socktype)
+    }
+}
